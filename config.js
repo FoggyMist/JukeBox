ddcec597f1f03d5458da76f06e906e671037d800
Foggy Mist <m.jurga@timecamp.com>
2016-10-22 17:40:07 +0200

    Ignored config.js file

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    botApiKey: 'slackbotApiKey',
-    youtubeApiKey: 'youtubeApiKey',
-    homeChannel: 'main bot's channelId',
+    botApiKey: 'xoxb-84839658627-jzFpgoaOU8KDwwGYbYIbL4JR',
+    youtubeApiKey: 'AIzaSyCtG5P3jc6f458FTDoqktJUVVQ4qv_P9PQ',
+    homeChannel: 'C2QQU4RAQ',
     admins: [],
 };
